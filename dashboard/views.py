dc525b345e59fe9f76cb886f3ebbbab8c21af0cd
Juan-RG <805001@unizar.es>
2024-10-05 16:20:11 +0200

    Improving the pannel view and Improving the user location

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def dashboard(request):
+  print("Hello")
   template = loader.get_template('dashboard.html')
   return HttpResponse(template.render())

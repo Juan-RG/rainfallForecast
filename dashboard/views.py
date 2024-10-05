86951cf771a41109a4cad3c292aeb20ad58d144f
Faizan Abbas <faizankhuwaja5@gmail.com>
2024-10-05 16:06:51 +0100

    home_test

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+def home(request):
+  print("Hello")
+  template = loader.get_template('home.html')
+  return HttpResponse(template.render())
+
 def dashboard(request):
   print("Hello")
   template = loader.get_template('dashboard.html')

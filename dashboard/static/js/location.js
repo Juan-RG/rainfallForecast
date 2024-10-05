a7c83af898f5b1586ab44e1d8a74e9ad4556a08a
Juan-RG <805001@unizar.es>
2024-10-05 10:37:00 +0200

    Feature: I create the location.js that extract the actual coordinates of your position and extract the city with these. I included the bootstrap remote style to have a more confortable up, I create a query form to send the data to the backed

---
+++ dashboard/static/js/location.js
@@ -0,0 +1,36 @@
+
+
+function getUserLocation() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          getCityName(latitude, longitude);
+        },
+        (error) => {
+          document.getElementById("locationOutput").textContent = `Error getting location: ${error.message}`;
+        }
+      );
+    } else {
+      document.getElementById("locationOutput").textContent = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  function getCityName(latitude, longitude) {
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const city = data.address.city || data.address.town || data.address.village;
+        //Set the form value in to the actual position
+        const locationInput = document.getElementById("actualLocation").value = city;
+        locationInput.value = city; 
+      })
+      .catch((error) => {
+        const locationInput = document.getElementById("actualLocation").value = `Error getting city name: ${error.message}`;
+      });
+  }
+
+  window.onload = getUserLocation;

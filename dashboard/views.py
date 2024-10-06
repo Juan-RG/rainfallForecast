cfaa53eda8838f8da7247a6ce68a5e247d4e4841
Juan-RG <805001@unizar.es>
2024-10-06 12:22:34 +0200

    Getting values of the API

--- dashboard/views.py
+++ dashboard/views.py
@@ -17,44 +17,89 @@ def dashboard(request):
 
     # Handle POST requests
     if request.method == 'POST':
-        print("aa")
         try:
-            print(request.body)
             data = json.loads(request.body)
-            min_lat = data.get('minLat')
-            max_lat = data.get('maxLat')
-            min_lng = data.get('minLng')
-            max_lng = data.get('maxLng')
-            location = data.get('location')
-            characteristics = data.get('characteristics')
-            date = data.get('date')
-            date = datetime.strptime(date, "%Y-%m-%d")
+            api_url, params = createRequest(data)
+            response = requests.get(api_url, params=params)
+            if response.status_code == 200:
+                response_text = response.text
+                print(response_text)
+                id = match = re.search(r'\["(.*?)"\]', response_text)
+                extracted_value = id.group(1)
+                print(extracted_value)
+                avrg = waitResult(extracted_value)
+                response_data = {
+                    'status': 'success',
+                    'message': 'Request received!',
+                    'received_data': data  # Optional: Echo back the received data
+                }
+            else:
+                print(f"Request failed with status code {response.status_code}")
+            return JsonResponse(response_data)  # Send back a JSON response
+                   
+            return JsonResponse(response_data)  # Send back a JSON response
+        except json.JSONDecodeError:
+            print("Entro2")
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+def waitResult(extracted_value):
+    api_url = 'https://climateserv.servirglobal.net/api/getDataRequestProgress/'
+    params = {
+        'id': extracted_value
+    }
+    progress = ""
+    while progress != "[100.0]":
+        progress = requests.get(api_url, params=params)
+        print(progress)
+        print(progress.text)
+        progress = progress.text
 
+    api_url = 'http://climateserv.servirglobal.net/api/getDataFromRequest/'
+    response = requests.get(api_url, params=params)
+    print(response)
+    print(response.text)
 
-            three_days_before = date - timedelta(days=3)
-            three_days_after = date + timedelta(days=3)
-            three_days_before = three_days_before.strftime("%m/%d/%Y")
-            three_days_after = three_days_after.strftime("%m/%d/%Y")
+    return response
+
+def createRequest(data):
+    min_lat = data.get('minLat')
+    max_lat = data.get('maxLat')
+    min_lng = data.get('minLng')
+    max_lng = data.get('maxLng')
+    location = data.get('location')
+    characteristics = data.get('characteristics')
+    date = data.get('date')
+    date = datetime.strptime(date, "%Y-%m-%d")
+
+    # Reduce the year by 1
+    date = date.replace(year=date.year - 1)
+
+    # Calculate three days before and after
+    three_days_before = date - timedelta(days=30)
+    three_days_after = date + timedelta(days=30)
+    print(three_days_before)
+    print(three_days_after)
+    three_days_before = three_days_before.strftime("%m/%d/%Y")
+    three_days_after = three_days_after.strftime("%m/%d/%Y")
 
             # Define the API URL
-            api_url = 'https://climateserv.servirglobal.net/api/submitDataRequest/'
+    api_url = 'https://climateserv.servirglobal.net/api/submitDataRequest/'
 
-            coordinates = [
+    coordinates = [
                 [min_lng, min_lat],  # Bottom-left corner
                 [min_lng, max_lat],  # Top-left corner
                 [max_lng, max_lat],  # Top-right corner
                 [max_lng, min_lat],  # Bottom-right corner
                 [min_lng, min_lat],  # Close the polygon by repeating the first point
             ]
-            geometry = {
+    geometry = {
                 "type": "Polygon",
                 "coordinates": [coordinates]
             }                        
             # Parameters to be added
-            params = {
+    params = {
                 'datatype': 0,
                 'begintime': three_days_before,
-                'endtime':  three_days_before,
+                'endtime':  three_days_after,
                 'intervaltype': 0,
                 'operationtype': 5,
                 'callback': 'successCallback',
@@ -62,24 +107,8 @@ def dashboard(request):
                 'isZip_CurrentDataType': 'false',
                 'geometry': json.dumps(geometry)
             }
-            response = requests.get(api_url, params=params)
-            response_text = response.text
-            print(response_text)
-            id = match = re.search(r'\["(.*?)"\]', response_text)
-            extracted_value = id.group(1)
-            print(extracted_value)                   
-            if response.status_code == 200:
-              response_data = {
-                  'status': 'success',
-                  'message': 'Request received!',
-                  'received_data': data  # Optional: Echo back the received data
-                }
-            else:
-                print(f"Request failed with status code {response.status_code}")
-            return JsonResponse(response_data)  # Send back a JSON response
-        except json.JSONDecodeError:
-            print("Entro2")
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
+    
+    return api_url,params
 
 
 ''''
